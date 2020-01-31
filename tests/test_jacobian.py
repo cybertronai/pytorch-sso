c272610af233dadef790ec5b9f94a94fb1495546
KazukiOsawa <osawa1021@gmail.com>
2020-02-01 05:48:22 +0900

    Update to measure time with JIT for JAX

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -1,60 +1,48 @@
+import sys
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-from opt_einsum import contract
 
 import flax
 import jax
+import jax.numpy as jnp
 
-import time
+from timeit import timeit
 
 
 class Net(nn.Module):
 
-    def __init__(self, n_inputs=3, n_outputs=10):
+    def __init__(self, n_inputs, n_outputs, hidden_ndim, n_layers):
         super().__init__()
-        self.fc1 = nn.Linear(n_inputs, 100, bias=False)
-        self.fc2 = nn.Linear(100, n_outputs, bias=False)
+        assert n_layers > 2
+        layers = [nn.Linear(n_inputs, hidden_ndim, bias=False), nn.ReLU()]
+        for i in range(n_layers - 2):
+            layers.append(nn.Linear(hidden_ndim, hidden_ndim, bias=False))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_ndim, n_outputs, bias=False))
+
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        s1 = self.fc1(x)
-        a1 = F.relu(s1)
-        s2 = self.fc2(a1)
-
-        return s2
-
-
-class ConvNet(nn.Module):
-
-    def __init__(self, n_outputs=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 128, 3, bias=False)
-        self.conv2 = nn.Conv2d(128, 256, 3, bias=False)
-        self.fc3 = nn.Linear(200704, n_outputs, bias=False)
-
-    def forward(self, x):
-        s1 = self.conv1(x)
-        a1 = F.relu(s1)
-        s2 = self.conv2(a1)
-        a2 = F.relu(s2)
-        a2 = a2.flatten(start_dim=1)
-        s3 = self.fc3(a2)
-
-        return s3
+        return self.layers(x)
 
 
 class FlaxNet(flax.nn.Module):
 
-    def apply(self, x):
-        s1 = flax.nn.Dense(x, features=100, bias=False)
-        a1 = flax.nn.relu(s1)
-        s2 = flax.nn.Dense(a1, features=1000, bias=False)
+    def apply(self, x, n_outputs, hidden_ndim, n_layers):
+        assert n_layers > 2
+        x = flax.nn.Dense(x, features=hidden_ndim, bias=False)
+        x = flax.nn.relu(x)
+        for i in range(n_layers - 2):
+            x = flax.nn.Dense(x, features=hidden_ndim, bias=False)
+            x = flax.nn.relu(x)
+        x = flax.nn.Dense(x, features=n_outputs, bias=False)
 
-        return s2
+        return x
 
 
 def register_hooks(model: nn.Module):
+
+    targets = (nn.Linear, nn.ReLU)
 
     def forward_postprocess(module, input, output):
         data_input = input[0]
@@ -62,203 +50,150 @@ def register_hooks(model: nn.Module):
         setattr(module, 'data_input', data_input)
         setattr(module, 'data_output', output)
 
-    for module in model.children():
-        module.register_forward_hook(forward_postprocess)
+    for module in model.modules():
+        if isinstance(module, targets):
+            module.register_forward_hook(forward_postprocess)
 
 
-def jax_jacobian(x, model, mode='fwd'):
-    output = model(x)
-    if mode == 'fwd':
-        Jx = jax.jacfwd(model)(x)
+def jax_jacobian(rng, input_shape, model, Jx_fn):
+    x = jax.random.normal(rng, input_shape)
+    y = model(x)
+    J = Jx_fn(x)
+
+    return J
+
+
+def manual_jacobian(bs, n_inputs, model, device, mode='rev'):
+    x = torch.randn(bs, n_inputs, device=device)
+    y = model(x)
+    modules = []
+    for module in model.modules():
+        if len(list(module.children())) > 0:
+            continue
+        modules.append(module)
+
+    if mode == 'rev':
+        J = manual_jacobian_rev(modules)
     else:
-        Jx = jax.jacrev(model)(x)
+        J = manual_jacobian_fwd(modules)
 
-    return Jx
-
-
-def manual_jacobian(x, model):
-    # x -> fc1 -> s1
-    # s1 -> relu -> a1
-    # a1 -> fc2 -> s2 (output)
-
-    register_hooks(model)
-    output = model(x)
-
-    n = output.size(0)
-    fc1 = model.fc1
-    fc2 = model.fc2
-
-    # Jacobian s2 (output) -> a1
-    Ja1 = torch.stack([fc2.weight] * n)
-
-    # ReLU derivative a1 -> s1
-    d = (fc2.data_input > 0).type(fc2.data_input.dtype)
-
-    # Jacobian s2 (output) -> s1
-    Js1 = contract('bij,bj->bij', Ja1, d)
-
-    # Jacobian s2 (output) -> x
-    Jx = contract('bij,jk->bik', Js1, fc1.weight)
-
-    return Jx
+    return J
 
 
-def manual_jacobian_conv(x, model):
-    # x -> conv1 -> s1
-    # s1 -> relu -> a1
-    # a1 -> conv2 -> s2
-    # s2 -> relu -> a2
-    # a2 -> fc3 -> s3 (output)
+def manual_jacobian_rev(modules):
+    J = None
 
-    bs = x.shape[0]
+    for module in modules[::-1]:
+        if J is None:
+            J = module.weight
+        elif isinstance(module, nn.ReLU):
+            a = module.data_output
+            d = (a > 0).type(a.dtype)
+            if J.ndimension() == 2:
+                J = torch.einsum('ab,nb->nab', J, d)
+            else:
+                J = torch.einsum('nab,nb->nab', J, d)
+        elif isinstance(module, nn.Linear):
+            J = torch.einsum('nab,bc->nac', J, module.weight)
 
-    register_hooks(model)
-    output = model(x)
-    n_outputs = output.shape[-1]
-
-    conv1 = model.conv1
-    conv2 = model.conv2
-    fc3 = model.fc3
-
-    # Jacobian s3 (output) -> a2
-    w3 = fc3.weight
-    Ja2 = torch.stack([w3] * bs)
-
-    # ReLU derivative a2 -> s2
-    a2 = fc3.data_input
-    d = (a2 > 0).type(a2.dtype)
-
-    # Jacobian s3 (output) -> s2
-    Js2 = contract('bij,bj->bij', Ja2, d)
-
-    # Jacobian s3 (output) -> a1
-    a1 = conv2.data_input
-    s2 = conv2.data_output
-    Js2 = Js2.reshape(bs * n_outputs, *s2.shape[1:])
-    Ja1 = F.grad.conv2d_input(a1.shape, conv2.weight, Js2)
-
-    # ReLU derivative a1 -> s1
-    d = (a1 > 0).type(a1.dtype)
-
-    # Jacobian s3 (output) -> s1
-    Ja1 = Ja1.reshape(bs, n_outputs, *a1.shape[1:])
-    Js1 = contract('bichw,bchw->bichw', Ja1, d)
-
-    # Jacobian s3 (output) -> x
-    s1 = conv1.data_output
-    Js1 = Js1.reshape(bs * n_outputs, *s1.shape[1:])
-    Jx = F.grad.conv2d_input(x.shape, conv1.weight, Js1)
-
-    return Jx
+    return J
 
 
-def reverse_mode_jacobian_with_repeat(x, model, bs=10, n_outputs=10):
+def manual_jacobian_fwd(modules):
+    J = None
+
+    for module in modules:
+        if J is None:
+            J = module.weight
+        elif isinstance(module, nn.ReLU):
+            a = module.data_output
+            d = (a > 0).type(a.dtype)
+            if J.ndimension() == 2:
+                J = torch.einsum('na,ab->nab', d, J)
+            else:
+                J = torch.einsum('na,nab->nab', d, J)
+        elif isinstance(module, nn.Linear):
+            J = torch.einsum('ab,nbc->nac', module.weight, J)
+
+    return J
+
+
+def reverse_mode_jacobian_with_repeat(bs, n_inputs, n_outputs, model, device):
+    x = torch.randn(bs, n_inputs, device=device)
+
     repeat_arg = (n_outputs,) + (1,) * len(x.size())
     xr = x.repeat(*repeat_arg)
     xr = xr.transpose(0, 1)
     xr.requires_grad_(True)
-    output = model(xr)
-    assert n_outputs == output.shape[-1]
+    y = model(xr)
     I = torch.eye(n_outputs, device=xr.device)
     I = I.repeat(bs, 1, 1)
 
-    Jx = torch.autograd.grad(output, xr, grad_outputs=I)[0]
+    Jx = torch.autograd.grad(y, xr, grad_outputs=I, retain_graph=True)[0]
 
     return Jx
 
 
-def reverse_mode_jacobian_with_repeat_conv(x, model, n_outputs=10):
-    bs = x.shape[0]
-    repeat_arg = (n_outputs,) + (1,) * len(x.size())
-    xr = x.repeat(*repeat_arg)
-    xr = xr.transpose(0, 1)
-    xr = xr.reshape(bs * n_outputs, *x.shape[1:])
-    xr.requires_grad_(True)
-    output = model(xr)
-    output = output.reshape(bs, n_outputs, -1)
-    assert n_outputs == output.shape[-1]
-    I = torch.eye(n_outputs, device=xr.device)
-    I = I.repeat(bs, 1, 1)
-
-    Jx = torch.autograd.grad(output, xr, grad_outputs=I)[0]
-
-    return Jx
-
-
-def test_jacobian():
-    bs = 1
+def main(mode):
+    bs = 32
     n_inputs = 1000
+    hidden_ndim = 1000
     n_outputs = 1000
+    n_layers = 10
     loop = 100
+    print('-------------')
+    print(f'mode: {mode}')
     print(f'bs: {bs}')
     print(f'n_inputs: {n_inputs}')
+    print(f'hidden_ndim: {hidden_ndim}')
     print(f'n_outputs: {n_outputs}')
-    print(f'loop: {loop}')
+    print(f'n_layers: {n_layers}')
     print('-------------')
-
-    rng = jax.random.PRNGKey(0)
-    x = jax.random.normal(rng, (bs, n_inputs))
-    _, model = FlaxNet.create(rng, x)
-
-    start = time.time()
-    for i in range(loop):
-        jax_jacobian(x, model, 'fwd')
-    elapsed = time.time() - start
-    print(f'jax.jacfwd: {elapsed:.3f}s')
-
-    start = time.time()
-    for i in range(loop):
-        jax_jacobian(x, model, 'rev')
-    elapsed = time.time() - start
-    print(f'jax.jacrev: {elapsed:.3f}s')
-
-    x = torch.randn(bs, n_inputs)
-    model = Net(n_inputs, n_outputs)
-
-    start = time.time()
-    for i in range(loop):
-        Jx_rev = reverse_mode_jacobian_with_repeat(x, model, bs, n_outputs)
-    elapsed = time.time() - start
-    print(f'reverse mode: {elapsed:.3f}s')
-
-    start = time.time()
-    for i in range(loop):
-        Jx_man = manual_jacobian(x, model)
-    elapsed = time.time() - start
-    print(f'manual mode: {elapsed:.3f}s')
-
-    print(f'(Jx_rev - Jx_man).max(): {(Jx_rev - Jx_man).max()}')
-
-
-def test_jacobian_conv():
-    bs = 128
-    n_outputs = 100
-    model = ConvNet(n_outputs)
-    x = torch.randn(bs, 3, 32, 32)
-    loop = 1
-
-    print(f'bs: {bs}')
-    print(f'input shape: {x.shape[1:]}')
-    print(f'n_outputs: {n_outputs}')
     print(f'loop: {loop}')
-    print('-------------')
 
-    start = time.time()
-    for i in range(loop):
-        Jx_rev = reverse_mode_jacobian_with_repeat_conv(x, model, n_outputs)
-    elapsed = time.time() - start
-    print(f'reverse mode: {elapsed:.3f}s')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    start = time.time()
-    for i in range(loop):
-        Jx_man = manual_jacobian_conv(x, model)
-    elapsed = time.time() - start
-    print(f'manual mode: {elapsed:.3f}s')
+    if mode == 'torch.auto':
+        # ------------------
+        # PyTorch auto-diff
+        print(f'device: {device}')
 
-    print(f'(Jx_rev - Jx_man).max(): {(Jx_rev - Jx_man).max()}')
+        model = Net(n_inputs, n_outputs, hidden_ndim, n_layers)
+        model = model.to(device)
+        args = [bs, n_inputs, n_outputs, model, device]
+        reverse_mode_jacobian_with_repeat(*args)
+        elapsed = timeit(lambda: reverse_mode_jacobian_with_repeat(*args), number=loop)
+        print(f'torch auto rev: {elapsed:.3f}s')
+    elif mode == 'torch.man':
+        # ------------------
+        # PyTorch manual-diff
+        print(f'device: {device}')
+
+        model = Net(n_inputs, n_outputs, hidden_ndim, n_layers)
+        model = model.to(device)
+        register_hooks(model)
+        args = [bs, n_inputs, model, device]
+        manual_jacobian(*args)
+        elapsed = timeit(lambda: manual_jacobian(*args), number=loop)
+        print(f'torch manual rev: {elapsed:.3f}s')
+    else:
+        # ------------------
+        # JAX
+        print(f'device: {jax.devices()}')
+
+        rng = jax.random.PRNGKey(0)
+        input_shape = (bs, n_inputs)
+        model_def = FlaxNet.partial(n_outputs=n_outputs, hidden_ndim=hidden_ndim, n_layers=n_layers)
+        _, model = model_def.create_by_shape(rng, [(input_shape, jnp.float32)])
+
+        jac_fun = jax.jit(jax.jacrev(model))
+        Jx_fn = jax.jit(jax.vmap(jac_fun, in_axes=(0,)))
+        args = [rng, input_shape, model, Jx_fn]
+        jax_jacobian(*args)
+        elapsed = timeit(lambda: jax_jacobian(*args), number=loop)
+        print(f'jit(jax.jacrev): {elapsed:.3f}s')
 
 
 if __name__ == '__main__':
-    test_jacobian()
-    print('*****************')
-    test_jacobian_conv()
+    mode = sys.argv[1]
+    main(mode)
